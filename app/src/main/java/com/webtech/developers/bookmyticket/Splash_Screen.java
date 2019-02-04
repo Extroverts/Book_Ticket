3a4e09fba81ab3fcee5ae0602873c6e6d0b429ec
Extroverts <onkarmahajan@gmail.com>
2019-02-04 10:46:35 +0530

    Time Frame 5 and 6 Splash Screen

---
+++ app/src/main/java/com/webtech/developers/bookmyticket/Splash_Screen.java
@@ -0,0 +1,38 @@
+package com.webtech.developers.bookmyticket;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Splash_Screen extends AppCompatActivity {
+
+
+    FirebaseAuth.AuthStateListener auth;
+    private FirebaseAuth Auth;
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_splash__screen );
+        Auth=FirebaseAuth.getInstance();
+final FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+        new Handler().postDelayed( new Runnable() {
+            @Override
+            public void run ( ) {
+                if(user==null){
+                    startActivity( new Intent( Splash_Screen.this,Login_Signup_Screen.class ) );
+                    finish();
+                }
+                else{
+                    startActivity( new Intent( Splash_Screen.this,MainActivity.class));
+                    finish();
+                }
+            }
+        }, 2000);
+    }
+}
